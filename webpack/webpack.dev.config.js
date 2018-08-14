ebb679d1ecab1238e16fe05e6483d5fa2763342e
Shauna <weisssa2@miamioh.edu>
2018-08-14 16:45:26 -0500

    added more scripts

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,27 +1,30 @@
+
 var webpack = require('webpack');
 var path = require('path');
 
+var parentDir = path.join(__dirname, '../');
+
 module.exports = {
-	entry: [
-		path.join(__dirname, '../index.js')
-	],
-	module: {
-		loaders: [{
-			test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},{
-				test: /\.less$/,
-				loaders: ["style-loader", "css-loader", "less-loader"]
-			}
-		]
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
     },
     output: {
-        path: __dirname + '/dist',
+        path: parentDir + '/dist',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: __dirname + '/dist',
+        contentBase: parentDir,
         historyApiFallback: true
     }
 }
