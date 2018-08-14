3f657d0e33dd5bf0ca0f1de7fd2ca462a28e1329
Shauna <weisssa2@miamioh.edu>
2018-08-14 17:09:44 -0500

    fixed dependencies, it is running

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,8 @@
+
 import React, {Component} from 'react';
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app</p>
+        return <p>It's not EaSy for everyone.</p>
     }
 }
