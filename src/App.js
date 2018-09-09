57cf6db2b4061c44096ae902c84c7b285eb169b3
Shauna Weiss <shauna.weiss@ibm.com>
2018-09-09 16:49:05 -0400

    added bootstrap to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+// import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 // import React, { Component } from 'react';
 // import logo from './logo.svg';
@@ -23,73 +23,115 @@ import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 // export default App;
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const BasicExample = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
+// const BasicExample = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/about">About</Link>
+//         </li>
+//         <li>
+//           <Link to="/topics">Topics</Link>
+//         </li>
+//         <li>
+//           <Link to="/topics">Contact Us</Link>
+//         </li>
+//       </ul>
 
-      <hr />
+//       <hr />
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
-);
+//       <Route exact path="/" component={Home} />
+//       <Route path="/about" component={About} />
+//       <Route path="/topics" component={Topics} />
+//     </div>
+//   </Router>
+// );
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
+// const Home = () => (
+//   <div>
+//     <h2>Home</h2>
+//   </div>
+// );
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
+// const About = () => (
+//   <div>
+//     <h2>About</h2>
+//   </div>
+// );
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
+// const Topics = ({ match }) => (
+//   <div>
+//     <h2>Topics</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>Components</Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//       </li>
+//     </ul>
 
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
+//     <Route path={`${match.url}/:topicId`} component={Topic} />
+//     <Route
+//       exact
+//       path={match.url}
+//       render={() => <h3>Please select a topic.</h3>}
+//     />
+//   </div>
+// );
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
+// const Topic = ({ match }) => (
+//   <div>
+//     <h3>{match.params.topicId}</h3>
+//   </div>
+// );
 
-export default BasicExample;
+// export default BasicExample;
+
+import React, { Component } from 'react';
+import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar inverse fixedTop>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React App</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+          </Grid>
+        </Navbar>
+        <Jumbotron>
+          <Grid>
+            <h1>Welcome to React</h1>
+            <p>
+              <Button
+                bsStyle="success"
+                bsSize="large"
+                href="http://react-bootstrap.github.io/components.html"
+                target="_blank"
+              >
+                View React Bootstrap Docs
+              </Button>
+            </p>
+          </Grid>
+        </Jumbotron>
+      </div>
+    );
+  }
+}
+
+export default App;
